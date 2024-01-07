cbf5959318a0f439b69cef25d7dee82f3b2d471b
Jordan Lim <jorized@gmail.com>
2024-01-07 23:59:20 +0800

    first commit - committing all files to the repository

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaView, ScrollView, StatusBar, StyleSheet } from 'react-native';
+import PokemonCard from './components/PokemonCard';
 
 export default function App() {
+
+  const charmanderData = {
+    name: "Charmander",
+    image: require("./assets/charmander.png"),
+    type: "Fire",
+    hp: 39,
+    moves: ["Scratch", "Ember", "Growl", "Leer"],
+    weaknesses: ["Water", "Rock"],
+    description: "Charmander is a small, bipedal, lizard-like Pokémon. Most of its body is colored orange, while its underbelly is a light yellow color. Charmander, along with all of its evolved forms, has a flame that is constantly burning on the end of its tail."
+  };
+
+  const squirtleData = {
+    name: "Squirtle",
+    image: require("./assets/squirtle.png"), // Replace with the actual image path
+    type: "Water",
+    hp: 44,
+    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+    weaknesses: ["Electric", "Grass"],
+    description: "Squirtle, known as the Tiny Turtle Pokémon, are turtle Pokémon with large eyes and chubby cheeks, capable of moving either on two feet or on all fours. Their skin is a light blue, and they possess a long, curled tail. When feeling threatened, Squirtle withdraw their limbs into their brown-orange shells and spray water from their mouth with great force, either to attack their opponent or merely to intimidate it. If attacked anyway, their shells are resilient, and provide excellent protection."
+  };
+
+  const bulbasaurData = {
+    name: "Bulbasaur",
+    image: require("./assets/bulbasaur.png"), // Replace with the actual image path
+    type: "Grass",
+    hp: 45,
+    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+    description: "In the Pokémon franchise, Bulbasaur are small, squat amphibian and plant Pokémon that move on all four legs, and have blue-green bodies with darker blue-green spots. As a Bulbasaur undergoes evolution into Ivysaur and then later into Venusaur, the bulb on its back blossoms into a large flower."
+  };
+
+  const pikachuData = {
+    name: "Pikachu",
+    image: require("./assets/pikachu.png"), // Replace with the actual image path
+    type: "Electric",
+    hp: 35,
+    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+    weaknesses: ["Ground"],
+    description: "Pikachu was designed around the concept of electricity. They are creatures that have short, yellow fur with brown markings covering their backs and parts of their lightning bolt-shaped tails. They have black-tipped, pointed ears and red circular pouches on their cheeks, which can spark with electricity."
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <PokemonCard {...charmanderData} />
+        <PokemonCard {...squirtleData} />
+        <PokemonCard {...bulbasaurData} />
+        <PokemonCard {...pikachuData} />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: StatusBar.currentHeight
   },
 });
---
+++ components/PokemonCard.js
@@ -0,0 +1,140 @@
+import { useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import FlipCard from 'react-native-flip-card'
+
+const getTypeDetails = (type) => {
+    switch (type.toLowerCase()) {
+      case "electric":
+        return { borderColor: "#FFD700", emoji: "⚡️" };
+      case "water":
+        return { borderColor: "#6493EA", emoji: "💧" };
+      case "fire":
+        return { borderColor: "#FF5733", emoji: "🔥" };
+      case "grass":
+        return { borderColor: "#66CC66", emoji: "🌿" };
+      default:
+        return { borderColor: "#A0A0A0", emoji: "❓" };
+    }
+};
+
+export default function PokemonCard({name, image, type, hp, moves, weaknesses, description}) {
+
+    const { borderColor, emoji } = getTypeDetails(type);
+
+    return (
+        <FlipCard clickable={true} flipHorizontal={true} flipVertical={false}>
+            {/*Front*/}
+            <View style = {styles.card}>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.name}>{name}</Text>
+                    <Text style={styles.hp}>❤️HP: {hp}</Text>
+                </View>
+
+                <Image style={styles.image} source={image} accessibilityLabel={`${name} pokemon`} resizeMode="contain"/>
+
+                <View style={styles.typeContainer}>
+                    <View style={[styles.badge, { borderColor }]}>
+                        <Text style={styles.typeEmoji}>{emoji}</Text>
+                        <Text style={styles.typeText}>{type}</Text>
+                    </View>
+                </View>
+
+                <View style={styles.movesContainer}>
+                    <Text style={styles.movesText}>Moves: {moves.join(", ")}</Text>
+                </View>
+
+                <View style={styles.weaknessContainer}>
+                    <Text style={styles.weaknessText}>
+                    Weakness: {weaknesses.join(", ")}
+                    </Text>
+                </View>
+
+            </View>
+            {/*Back*/}
+            <View style = {styles.card}>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.name}>Description:</Text>
+                    <Text style={styles.hp}>📙</Text>
+                </View>
+                <View style={styles.movesContainer}>
+                    <Text style={styles.movesText}>{description}</Text>
+                </View>
+            </View>
+        </FlipCard>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: "white",
+        borderRadius: 16,
+        borderWidth: 2,
+        padding: 16,
+        margin: 16,
+        ...Platform.select({
+            ios: {
+                shadowOffSet: { width: 2, height: 2},
+                shadowColor: "#333",
+                shadowOpacity: 0.3,
+                shadowRadius: 4,
+            },
+            android: {
+                elevation: 5
+            }
+        })
+    },
+    nameContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 32
+    },
+    name: {
+        fontSize: 30,
+        fontWeight: "bold"
+    },
+    hp: {
+        fontSize: 22
+    },
+    image: {
+        width: "100%",
+        height: 300,
+        marginBottom: 16,
+    },
+    typeContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: 40
+    },
+    badge: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingVertical: 6,
+        paddingHorizontal: 12,
+        borderRadius: 20,
+        borderWidth: 4,
+    },
+    typeEmoji: {
+        fontSize: 30,
+        marginRight: 12,
+    },
+    typeText: {
+        fontSize: 22,
+        fontWeight: "bold",
+    },
+    movesContainer: {
+        marginBottom: 12,
+    },
+    movesText: {
+        fontSize: 22,
+        fontWeight: "bold",
+    },
+    weaknessContainer: {
+        marginBottom: 8,
+    },
+    weaknessText: {
+        fontSize: 22,
+        fontWeight: "bold",
+    },
+})
